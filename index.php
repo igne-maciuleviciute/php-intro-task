33259fb75c6b143589ec0a3b73e00f4cddc6146d
igne-maciuleviciute <igne.maciuleviciute2@gmail.com>
2018-10-13 17:10:00 +0300

    init commit

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+use function Nfq\Akademija\calculateHomeWorkSum;
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+calculateHomeWorkSum();
+//\Nfq\Akademija\Not_Typed\calculateHomeWorkSum();
+//\Nfq\Akademija\Soft\calculateHomeWorkSum();
+//\Nfq\Akademija\Strict\calculateHomeWorkSum();
---
+++ src/functions.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Nfq\Akademija;
+
+function calculateHomeWorkSum(){
+    printf("root\n");
+}
+//
+//namespace Nfq\Akademija\Not_Typed;
+//
+//function calculateHomeWorkSum(){
+//    printf("nottyped\n");
+//}
+//
+//namespace Nfq\Akademija\Soft;
+//
+//function calculateHomeWorkSum(){
+//    printf("soft\n");
+//}
+//
+//namespace Nfq\Akademija\Strict;
+//
+//function calculateHomeWorkSum(){
+//    printf("strict\n");
+//}
+
