836846de1ff109c62b2712cb6aba3e0e51859ca2
igne-maciuleviciute <igne.maciuleviciute2@gmail.com>
2018-10-14 14:34:32 +0300

    update strict function

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
+
 require_once __DIR__ . '/vendor/autoload.php';
 
 printf(\Nfq\Akademija\calculateHomeWorkSum(3, 2.2, '1')."\n\n");
 printf(\Nfq\Akademija\Not_Typed\calculateHomeWorkSum(3, 2.2, '1')."\n\n");
 printf(\Nfq\Akademija\Soft\calculateHomeWorkSum(3, 2.2, '1')."\n\n");
-printf(\Nfq\Akademija\Strict\calculateHomeWorkSum(3, 2.2, '1')."\n\n");
+printf(\Nfq\Akademija\Strict\callCalculateHomeWorkSum(3, 2.2, '1')."\n\n");
--- src/functions_strict.php
+++ src/functions_strict.php
@@ -4,15 +4,14 @@ declare(strict_types=1);
 namespace Nfq\Akademija\Strict;
 
 
-
-function calculateHomeWorkSum(int $a, float $b, string $c): int
+function calculateHomeWorkSum(int $a, int $b, int $c): int
 {
     return $a + $b + $c;
-
-    function callStrictTypeCalculation() {
-        $this -> calculateHomeWorkSum();
-    }
 }
 
+function callCalculateHomeWorkSum($a, $b,  $c)
+{
+    return calculateHomeWorkSum($a, $b, $c);
+}
 
 
